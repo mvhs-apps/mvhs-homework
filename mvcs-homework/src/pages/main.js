52e9ec7225030da11f555ddff9b53ca0c9d68a95
AlexJVG <mrrocksminecraft@gmail.com>
2017-11-15 16:28:12 -0800

    Andrew Code v2
    Thank you Andrew for the code

--- mvcs-homework/src/pages/main.js
+++ mvcs-homework/src/pages/main.js
@@ -9,31 +9,47 @@ var name = (cookies.get('name'));
 var key = (cookies.get('key'));
 console.log(key);
 
-class Alex {
+class Homework {
   async loadTest() {
     try {
       const response = await fetch('https://classroom.googleapis.com/v1/courses?access_token=' + key);
       const json = await response.json();
       console.log(json);
-      document.getElementById('root').innerHTML = json.courses[1].name;
-      //In the next fetch line you must specify which course you want to access. Send me a slack message if you need help. -Alex
-      const coursework = await fetch('https://classroom.googleapis.com/v1/courses/' +json.courses[3].id+'/courseWork?access_token='+key)
-      const coursejson = await coursework.json();
-      console.log(coursejson);
+      var ctext ="";
+      var counter1 = 0;
+      var coursework;
+      var coursejson;
+      var courses = [];
+      var counter2 = 0;
+      for(counter1 in json.courses){
+        coursework = await fetch('https://classroom.googleapis.com/v1/courses/' +json.courses[counter1].id+'/courseWork?access_token='+key);
+        coursejson = await coursework.json();
+        courses[counter1]=coursejson;
+      }
+      console.log(courses);
+      counter1 = 0;
+      for(counter2 in courses){
+        ctext += (Number(counter2)+1)+'. '+'<b>'+json.courses[counter2].name+'</b>'+':'+'<br/>';
+        for(counter1 in courses[counter2].courseWork){
+          ctext += "<p>"+courses[counter2].courseWork[counter1].title+"</p>";
+        }
+      }
+      document.getElementById('root').innerHTML = 'This is the work in each of your classes:'+'<br/>'+ctext;
       } catch(err) {
         console.log(err);
     }
   }
 }
 
-var al = new Alex();
-al.loadTest();
+var app = new Homework();
+app.loadTest();
 
 const LoginPage = () =>
   <div>
     <h1>Welcome {name}</h1>
 
     <p id = "root">If this shows then we have a problem or the courses are in console</p>
+    <p id = "root1"></p>
   </div>
 
 export default LoginPage
