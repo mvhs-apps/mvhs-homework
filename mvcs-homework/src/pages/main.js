8c529b4f2a2f6e0788297deec010c74f96f2dbd1
AndrewRunke <andrewrunke@gmail.com>
2017-11-17 12:37:55 -0800

    DONE

--- mvcs-homework/src/pages/main.js
+++ mvcs-homework/src/pages/main.js
@@ -15,7 +15,10 @@ class Homework {
       const response = await fetch('https://classroom.googleapis.com/v1/courses?access_token=' + key);
       const json = await response.json();
       console.log(json);
-      var ctext ="";
+      var coursemax = 4;
+      var length = coursemax*8;
+      var ctext = [length];
+      var ctext2 = [length];
       var counter1 = 0;
       var coursework;
       var coursejson;
@@ -29,12 +32,18 @@ class Homework {
       console.log(courses);
       counter1 = 0;
       for(counter2 in courses){
-        ctext += (Number(counter2)+1)+'. '+'<b>'+json.courses[counter2].name+'</b>'+':'+'<br/>';
         for(counter1 in courses[counter2].courseWork){
-          ctext += "<p>"+courses[counter2].courseWork[counter1].title+"</p>";
+          if (counter1<coursemax){
+            ctext[counter2] = "<p>"+courses[counter2].courseWork[counter1].title+"</p>";
+          }
         }
+        document.getElementById('rootname'+counter2).innerHTML = json.courses[counter2].name+'</b>'+':'+'<br/>';;
+
       }
-      document.getElementById('root').innerHTML = 'This is the work in each of your classes:'+'<br/>'+ctext;
+      for(counter2 in ctext){
+        document.getElementById('root'+counter2).innerHTML = ctext[counter2];
+
+      }
       } catch(err) {
         console.log(err);
     }
@@ -47,8 +56,22 @@ app.loadTest();
 const LoginPage = () =>
   <div>
     <h1>Welcome {name}</h1>
-
-    <p id = "root">If this shows then we have a problem or the courses are in console</p>
+    <p id = "rootname0"></p>
+    <p id = "root0"></p>
+    <p id = "rootname1"></p>
+    <p id = "root1"></p>
+    <p id = "rootname2"></p>
+    <p id = "root2"></p>
+    <p id = "rootname3"></p>
+    <p id = "root3"></p>
+    <p id = "rootname4"></p>
+    <p id = "root4"></p>
+    <p id = "rootname5"></p>
+    <p id = "root5"></p>
+    <p id = "rootname6"></p>
+    <p id = "root6"></p>
+    <p id = "rootname7"></p>
+    <p id = "root7"></p>
   </div>
 
 export default LoginPage
