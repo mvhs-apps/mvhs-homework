fe0ffe4f61ad611836ac19efab4c3a2aa0c6dd2d
Akhand Dugar <akhand.dugar@gmail.com>
2017-12-03 11:07:24 -0800

    Code with errors
    Please run the code, see the error, and give any feedback. Possible page for help:
    
    https://stackoverflow.com/questions/26566317/invariant-violation-registercomponent-target-container-is-not-a-dom-elem#

--- mvcs-homework/src/pages/main.js
+++ mvcs-homework/src/pages/main.js
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom'
 import Link from 'gatsby-link';
 import Cookies from 'universal-cookie';
 import "isomorphic-fetch";
+import { render } from 'react-dom';
+import './style.css';
+import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
+import {amber,blue,red} from 'material-ui/colors';
+import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+import Typography from 'material-ui/Typography';
+import Tabs, { Tab } from 'material-ui/Tabs';
+import PropTypes from 'prop-types';
+import { withStyles } from 'material-ui/styles';
+import Table, { TableBody, TableCell, TableHead, TableRow } from 'material-ui/Table';
+import Paper from 'material-ui/Paper';
+import Avatar from 'material-ui/Avatar';
+import DateRangeIcon from 'material-ui-icons/DateRange';
+import AssignmentIcon from 'material-ui-icons/Assignment';
+import NoteIcon from 'material-ui-icons/Note';
 
+//Logic code
 const cookies = new Cookies();
 
 var name = (cookies.get('name'));
@@ -16,14 +34,13 @@ class Homework {
       const json = await response.json();
       console.log(json);
       var coursemax = 4;
-      var length = coursemax;
-      var ctext = [length];
-      var ctext2 = [length];
+      var ctext = [coursemax*8];
+      var ctext2 = [coursemax*8];
       var counter1 = 0;
+      var counter2 = 0;
       var coursework;
       var coursejson;
       var courses = [];
-      var counter2 = 0;
       for(counter1 in json.courses){
         coursework = await fetch('https://classroom.googleapis.com/v1/courses/' +json.courses[counter1].id+'/courseWork?access_token='+key);
         coursejson = await coursework.json();
@@ -32,19 +49,17 @@ class Homework {
       console.log(courses);
       counter1 = 0;
       for(counter2 in courses){
-        ctext[counter2] = "";
         for(counter1 in courses[counter2].courseWork){
-          
-          if (counter1<coursemax){
-            ctext[counter2] += "<p>"+courses[counter2].courseWork[counter1].title+"</p>";
+          if (counter1<=coursemax){
+            ctext[counter1] = "<p>"+courses[counter2].courseWork[counter1].title+"</p>";
           }
         }
         document.getElementById('rootname'+counter2).innerHTML = json.courses[counter2].name+'</b>'+':'+'<br/>';;
 
       }
-
       for(counter2 in ctext){
         document.getElementById('root'+counter2).innerHTML = ctext[counter2];
+
       }
       } catch(err) {
         console.log(err);
@@ -52,28 +67,107 @@ class Homework {
   }
 }
 
-var app = new Homework();
-app.loadTest();
+//MUI and rendering code
 
-const LoginPage = () =>
-  <div>
-    <h1>Welcome {name}</h1>
-    <p id = "rootname0">Loading</p>
-    <p id = "root0"></p>
-    <p id = "rootname1"></p>
-    <p id = "root1"></p>
-    <p id = "rootname2"></p>
-    <p id = "root2"></p>
-    <p id = "rootname3"></p>
-    <p id = "root3"></p>
-    <p id = "rootname4"></p>
-    <p id = "root4"></p>
-    <p id = "rootname5"></p>
-    <p id = "root5"></p>
-    <p id = "rootname6"></p>
-    <p id = "root6"></p>
-    <p id = "rootname7"></p>
-    <p id = "root7"></p>
-  </div>
+function TabContainer(props) {
+  return <div style={{ padding: 8 * 3 }}>{props.children}</div>;
+}
 
-export default LoginPage
+const theme = createMuiTheme ({
+      palette: {
+        primary: amber,
+        secondary: blue
+      },
+      root: {
+      flexGrow: 1,
+      },
+
+  });
+
+class CenteredTabs extends React.Component {
+  state = {
+    value: 0,
+  };
+}
+
+class App extends Component {
+  
+  constructor() {
+    super();
+    this.state = {
+      value: 0
+    };
+  };
+    
+    handleChange = (event, value) => {
+    this.setState({ value: value });
+  };
+  
+  render() {
+    const { div } = this.props;
+    const { value } = this.state;
+    return (
+      <MuiThemeProvider theme={theme}>
+      <div className={div}>
+        <AppBar position="static" id='title'>
+          <Toolbar>
+            <Typography type="title" color='inherit'>
+              MVHS Homework App
+            </Typography>
+            <div id='avatar' color='inherit'><Avatar>A</Avatar></div>
+          </Toolbar>
+          <Tabs value={value} onChange={this.handleChange} centered>
+              <Tab label= {<AssignmentIcon />} id='icon'/>
+              <Tab label= {<NoteIcon />} id='icon'/>
+        </Tabs>
+        </AppBar>
+        {value === 0 && <TabContainer>
+        <Paper id='calendar'>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell><p id = "rootname1"></p></TableCell>
+                <TableCell><p id = "root1"></p></TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell><p id = "rootname2"></p></TableCell>
+                <TableCell><p id = "root2"></p></TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell><p id = "rootname3"></p></TableCell>
+                <TableCell><p id = "root3"></p></TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell><p id = "rootname4"></p></TableCell>
+                <TableCell><p id = "root4"></p></TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell><p id = "rootname5"></p></TableCell>
+                <TableCell><p id = "root5"></p></TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell><p id = "rootname6"></p></TableCell>
+                <TableCell><p id = "root6"></p></TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell><p id = "rootname7"></p></TableCell>
+                <TableCell><p id = "root7"></p></TableCell>
+              </TableRow>
+            </TableHead>
+          </Table>
+        </Paper>
+        </TabContainer>}
+        {value === 1 && <TabContainer>
+        There is no API for Google Keep... Sadness...
+        </TabContainer>}
+      </div>
+    </MuiThemeProvider>
+    );
+  }
+}
+
+CenteredTabs.propTypes = {
+  div: PropTypes.object.isRequired,
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
