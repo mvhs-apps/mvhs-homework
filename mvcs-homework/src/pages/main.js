ee49a78b152ce80ca42834f3ca3107126e17f3d9
Akhand Dugar <akhand.dugar@gmail.com>
2017-12-12 11:08:06 -0800

    stuff
    stuff

--- mvcs-homework/src/pages/main.js
+++ mvcs-homework/src/pages/main.js
@@ -22,58 +22,14 @@ import NoteIcon from 'material-ui-icons/Note';
 
 //Logic code
 const cookies = new Cookies();
-
 var name = (cookies.get('name'));
 var key = (cookies.get('key'));
 console.log(key);
 
-class Homework {
-  async loadTest() {
-    try {
-      const response = await fetch('https://classroom.googleapis.com/v1/courses?access_token=' + key);
-      const json = await response.json();
-      console.log(json);
-      var coursemax = 4;
-      var ctext = [coursemax * 8];
-      var ctext2 = [coursemax * 8];
-      var counter1 = 0;
-      var counter2 = 0;
-      var coursework;
-      var coursejson;
-      var courses = [];
-      for (counter1 in json.courses) {
-        coursework = await fetch('https://classroom.googleapis.com/v1/courses/' + json.courses[counter1].id + '/courseWork?access_token=' + key);
-        coursejson = await coursework.json();
-        courses[counter1] = coursejson;
-      }
-      console.log(courses);
-      counter1 = 0;
-      for (counter2 in courses) {
-        for (counter1 in courses[counter2].courseWork) {
-          if (counter1 <= coursemax) {
-            ctext[counter1] = "<p>" + courses[counter2].courseWork[counter1].title + "</p>";
-          }
-        }
-        document.getElementById('rootname' + counter2).innerHTML = json.courses[counter2].name + '</b>' + ':' + '<br/>';;
-
-      }
-      for (counter2 in ctext) {
-        if (document.readyState === 'complete') {
-          document.getElementById('root' + counter2).innerHTML = ctext[counter2];
-        }
-      }
-    } catch (err) {
-      console.log(err);
-    }
-  }
-}
-
 //MUI and rendering code
-
 function TabContainer(props) {
   return <div style={{ padding: 8 * 3 }}>{props.children}</div>;
 }
-
 const theme = createMuiTheme({
   palette: {
     primary: amber,
@@ -82,51 +38,134 @@ const theme = createMuiTheme({
   root: {
     flexGrow: 1,
   },
-
 });
-
 class CenteredTabs extends React.Component {
   state = {
     value: 0,
   };
 }
-
 class App extends Component {
-
   constructor() {
     super();
     this.state = {
-      value: 0
+      value: 0,
+      root: "loading..."
     };
   };
-
   handleChange = (event, value) => {
     this.setState({ value: value });
   };
+  componentDidMount() {
+    async function loadTest() {
+      try {
+        const response = await fetch('https://classroom.googleapis.com/v1/courses?access_token=' + key);
+        const json = await response.json();
+        console.log(json);
+        var coursemax = 4;
+        var length = coursemax;
+        var ctext = [length];
+        var ctext2 = [length];
+        var counter1 = 0;
+        var coursework;
+        var coursejson;
+        var courses = [];
+        var counter2 = 0;
+        for (counter1 in json.courses) {
+          coursework = await fetch('https://classroom.googleapis.com/v1/courses/' + json.courses[counter1].id + '/courseWork?access_token=' + key);
+          coursejson = await coursework.json();
+          courses[counter1] = coursejson;
+        }
+        console.log(courses);
+        counter1 = 0;
+        for (counter2 in courses) {
+          ctext[counter2] = "";
+          for (counter1 in courses[counter2].courseWork) {
+
+            if (counter1 < coursemax) {
+              ctext[counter2] += "<p>" + courses[counter2].courseWork[counter1].title + "</p>";
+            }
+          }
+          document.getElementById('rootname' + counter2).innerHTML = json.courses[counter2].name + '</b>' + ':' + '<br/>';;
+        }
+        for (counter2 in ctext) {
+          document.getElementById('root' + counter2).innerHTML = ctext[counter2];
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    loadTest();
+  }
+
+  componentDidMount() {
+    async function loadTest() {
+      try {
+        const response = await fetch('https://classroom.googleapis.com/v1/courses?access_token=' + key);
+      const json = await response.json();
+      console.log(json);
+      var coursemax = 4;
+      var length = coursemax;
+      var ctext = [length];
+      var ctext2 = [length];
+      var counter1 = 0;
+      var coursework;
+      var coursejson;
+      var courses = [];
+      var counter2 = 0;
+      for(counter1 in json.courses){
+        coursework = await fetch('https://classroom.googleapis.com/v1/courses/' +json.courses[counter1].id+'/courseWork?access_token='+key);
+        coursejson = await coursework.json();
+        courses[counter1]=coursejson;
+      }
+      console.log(courses);
+      counter1 = 0;
+      for(counter2 in courses){
+        ctext[counter2] = "";
+        for(counter1 in courses[counter2].courseWork){
+          
+          if (counter1<coursemax){
+            ctext[counter2] += "<p>"+courses[counter2].courseWork[counter1].title+"</p>";
+          }
+        }
+        document.getElementById('rootname'+counter2).innerHTML = json.courses[counter2].name+'</b>'+':'+'<br/>';;
+
+      }
+
+      for(counter2 in ctext){
+        document.getElementById('root'+counter2).innerHTML = ctext[counter2];
+      }
+      } catch(err) {
+        console.log(err);
+    }
+    }
+    loadTest();
+  }
 
   render() {
     const { div } = this.props;
     const { value } = this.state;
 
     return (
-      <MuiThemeProvider theme={theme}>
+      <MuiThemeProvider theme={theme} >
         <div className={div}>
-          <AppBar position="static" id='title'>
-            <Toolbar>
-              <Typography type="title" color='inherit'>
-                MVHS Homework App
-            </Typography>
-              <div id='avatar' color='inherit'><Avatar>A</Avatar></div>
-            </Toolbar>
-            <Tabs value={value} onChange={this.handleChange} centered>
-              <Tab label={<AssignmentIcon />} id='icon' />
-              <Tab label={<NoteIcon />} id='icon' />
-            </Tabs>
-          </AppBar>
+          <div id='title'>
+            <AppBar position="static">
+              <div id='avatar'><Avatar>{name.charAt(0)}</Avatar></div>
+              <Tabs value={value} onChange={this.handleChange} centered>
+                <Tab label={<AssignmentIcon />} id='icon' />
+                <Tab label={<NoteIcon />} id='icon' />
+              </Tabs>
+            </AppBar>
+          </div>
           {value === 0 && <TabContainer>
-            <Paper id='calendar'>
+          <div id='calendar'>
+            <Paper>
               <Table>
                 <TableHead>
+                  <TableRow>
+                    <TableCell><p id="rootname0">Loading... </p></TableCell>
+                    <TableCell><p id="root0"></p></TableCell>
+                  </TableRow>
                   <TableRow>
                     <TableCell><p id="rootname1"></p></TableCell>
                     <TableCell><p id="root1"></p></TableCell>
@@ -151,13 +190,10 @@ class App extends Component {
                     <TableCell><p id="rootname6"></p></TableCell>
                     <TableCell><p id="root6"></p></TableCell>
                   </TableRow>
-                  <TableRow>
-                    <TableCell><p id="rootname7"></p></TableCell>
-                    <TableCell><p id="root7"></p></TableCell>
-                  </TableRow>
                 </TableHead>
               </Table>
             </Paper>
+          </div>
           </TabContainer>}
           {value === 1 && <TabContainer>
             There is no API for Google Keep... Sadness...
@@ -169,7 +205,7 @@ class App extends Component {
 }
 
 CenteredTabs.propTypes = {
-  div: PropTypes.object.isRequired,
+div: PropTypes.object.isRequired,
 };
 
 export default App;
