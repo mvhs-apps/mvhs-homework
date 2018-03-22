f7ccef4fe96fdf8e37625adb5a0164b7694dc9e0
Akhand Dugar <akhand.dugar@gmail.com>
2018-03-22 09:17:21 -0700

    Now it's pretty and it works!
    Yay

--- mvcs-homework/src/pages/index.js
+++ mvcs-homework/src/pages/index.js
@@ -1,8 +1,35 @@
-import React from 'react';
 import GoogleLogin from 'react-google-login';
 import Cookies from 'universal-cookie';
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import './style.css';
+import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
+import {amber,blue,red} from 'material-ui/colors';
+import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+import Typography from 'material-ui/Typography';
+import Tabs, { Tab } from 'material-ui/Tabs';
+import PropTypes from 'prop-types';
+import { withStyles } from 'material-ui/styles';
+import Table, { TableBody, TableCell, TableHead, TableRow } from 'material-ui/Table';
+import Paper from 'material-ui/Paper';
+import Avatar from 'material-ui/Avatar';
+import DateRangeIcon from 'material-ui-icons/DateRange';
+import AssignmentIcon from 'material-ui-icons/Assignment';
+import NoteIcon from 'material-ui-icons/Note';
 
 const cookies = new Cookies();
+
+const theme = createMuiTheme ({
+  palette: {
+    primary: amber,
+    secondary: blue
+  },
+  root: {
+  flexGrow: 1,
+  },
+
+});
 
 //cookies.set('myCat', 'Pacman', { path: '/' });
 //console.log(cookies.get('myCat')); Pacman
@@ -24,18 +51,49 @@ const failureResponse = (response) => {
 }
 
 const IndexPage = () =>
-  <div>
-    <h1>Login to your account</h1>
-    <p>Don't worry we won't just take all your passwords</p>
-    <GoogleLogin
-      clientId="290518323911-f5ph5ghp1qr230r90f79p7sg2n5o0vn9.apps.googleusercontent.com"
-      scope="https://www.googleapis.com/auth/classroom.courses https://www.googleapis.com/auth/classroom.coursework.me"
-      response_type="token"
-      prompt="select_account"
-      buttonText="Login"
-      onSuccess={responseGoogle}
-      onFailure={failureResponse}
-    />
-  </div>
+
+  <MuiThemeProvider theme={theme}>
+
+    <div>
+
+      <AppBar position="static" id='title'>
+
+        <Toolbar>
+
+          <Typography type="title" color='inherit'>
+
+            MVHS Homework App
+
+            </Typography>
+              <div id='button'>
+
+                <GoogleLogin
+
+                  clientId="290518323911-f5ph5ghp1qr230r90f79p7sg2n5o0vn9.apps.googleusercontent.com"
+
+                  scope="https://www.googleapis.com/auth/classroom.courses https://www.googleapis.com/auth/classroom.coursework.me"
+
+                  response_type="token"
+
+                  prompt="select_account"
+
+                  buttonText="Login"
+
+                  class="google"
+
+                  onSuccess={responseGoogle}
+
+                  onFailure={failureResponse} 
+
+                />
+              </div>
+
+        </Toolbar>
+
+      </AppBar>
+
+    </div>
+
+  </MuiThemeProvider>
 
 export default IndexPage
--- mvcs-homework/src/pages/main.js
+++ mvcs-homework/src/pages/main.js
@@ -1,11 +1,39 @@
-import React from 'react';
 import Link from 'gatsby-link';
 import Cookies from 'universal-cookie';
 import "isomorphic-fetch";
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import './style.css';
+import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
+import {amber,blue,red} from 'material-ui/colors';
+import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+import Typography from 'material-ui/Typography';
+import Tabs, { Tab } from 'material-ui/Tabs';
+import PropTypes from 'prop-types';
+import { withStyles } from 'material-ui/styles';
+import Table, { TableBody, TableCell, TableHead, TableRow } from 'material-ui/Table';
+import Paper from 'material-ui/Paper';
+import Avatar from 'material-ui/Avatar';
+import DateRangeIcon from 'material-ui-icons/DateRange';
+import AssignmentIcon from 'material-ui-icons/Assignment';
+import NoteIcon from 'material-ui-icons/Note';
+
 const cookies = new Cookies();
 var name = (cookies.get('name'));
 var key = (cookies.get('key'));
 console.log(key);
+
+const theme = createMuiTheme ({
+  palette: {
+    primary: amber,
+    secondary: blue
+  },
+  root: {
+  flexGrow: 1,
+  },
+
+});
 
 function isEmpty(obj) {
   for(var key in obj) {
@@ -43,23 +71,51 @@ class Homework {
 var app = new Homework();
 app.loadTest();
 const LoginPage = () =>
-  <div>
-    <h1>Welcome {name}</h1>
-    <p id="class1">Loading</p>
-    <p id="class1info"></p>
-    <p id="class2"></p>
-    <p id="class2info"></p>
-    <p id="class3"></p>
-    <p id="class3info"></p>
-    <p id="class4"></p>
-    <p id="class4info"></p>
-    <p id="class5"></p>
-    <p id="class5info"></p>
-    <p id="class6"></p>
-    <p id="class6info"></p>
-    <p id="class7"></p>
-    <p id="class7info"></p>
-    <p id="class8"></p>
-    <p id="class8info"></p>
-  </div>
+<MuiThemeProvider theme={theme}>
+      <div>
+        <AppBar position="static" id='title'>
+          <Toolbar>
+            <Typography type="title" color='inherit'>
+              MVHS Homework App
+            </Typography>
+            <div id='avatar' color='inherit'><Avatar>A</Avatar></div>
+          </Toolbar>
+        </AppBar>
+        <Paper id='calendar'>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell><p id = "class1"></p></TableCell>
+                <TableCell><p id = "class1info"></p></TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell><p id = "class2"></p></TableCell>
+                <TableCell><p id = "class2info"></p></TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell><p id = "class3"></p></TableCell>
+                <TableCell><p id = "class3info"></p></TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell><p id = "class4"></p></TableCell>
+                <TableCell><p id = "class4info"></p></TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell><p id = "class5"></p></TableCell>
+                <TableCell><p id = "class5info"></p></TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell><p id = "class6"></p></TableCell>
+                <TableCell><p id = "class6info"></p></TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell><p id = "class7"></p></TableCell>
+                <TableCell><p id = "class7info"></p></TableCell>
+              </TableRow>
+            </TableHead>
+          </Table>
+        </Paper>
+      </div>
+    </MuiThemeProvider>
+  
 export default LoginPage
