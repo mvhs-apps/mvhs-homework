3921896935b2861c88180091957044b543ceed00
John Park <KronosOfWarTime@gmail.com>
2018-03-21 21:01:34 -0700

    Rewrote Andrew's code

--- mvcs-homework/src/pages/main.js
+++ mvcs-homework/src/pages/main.js
@@ -7,34 +7,33 @@ var name = (cookies.get('name'));
 var key = (cookies.get('key'));
 console.log(key);
 
+function isEmpty(obj) {
+  for(var key in obj) {
+      if(obj.hasOwnProperty(key))
+          return false;
+  }
+  return true;
+}
+
 class Homework {
   async loadTest() {
     try {
       const response = await fetch('https://classroom.googleapis.com/v1/courses?access_token=' + key);
       const json = await response.json();
       console.log(json);
-      var coursemax = 4;
-      var ctext, ctext2 = [coursemax*8];
-      var counter1, counter2 = 0;
-      var coursewor, coursejson;
-      var courses = [];
-      for (counter1 in json.courses){
-        coursework = await fetch('https://classroom.googleapis.com/v1/courses/' +json.courses[counter1].id+'/courseWork?access_token='+key);
-        coursejson = await coursework.json();
-        courses[counter1]=coursejson;
-      }
-      console.log(courses);
-      counter1 = 0;
-      for(counter2 in courses){
-        for(counter1 in courses[counter2].courseWork){
-          if (counter1<=coursemax){
-            ctext[counter1] = "<p>"+courses[counter2].courseWork[counter1].title+"</p>";
-          }
+      for(var i = 0; i < json.courses.length; i++) {
+        //console.log("did for "+json.courses[i].name);
+        document.getElementById("class"+(i+1).toString()).innerHTML = json.courses[i].name;
+
+        var res2 = await fetch('https://classroom.googleapis.com/v1/courses/' +json.courses[i].id+'/courseWork?access_token='+key);
+        var json2 = await res2.json();
+        console.log(json2);
+        if(!isEmpty(json2)) {      
+          var final = "";
+          //console.log(json2.courseWork[0].title);
+          final += json2.courseWork[0].title;
+          document.getElementById("class"+(i+1).toString()+"info").innerHTML = final; 
         }
-        document.getElementById('rootname'+counter2).innerHTML = json.courses[counter2].name+'</b>'+':'+'<br/>';;
-      }
-      for(counter2 in ctext){
-        document.getElementById('root'+counter2).innerHTML = ctext[counter2];
       }
     } catch(err) {
       console.log(err);
@@ -46,21 +45,21 @@ app.loadTest();
 const LoginPage = () =>
   <div>
     <h1>Welcome {name}</h1>
-    <p id = "rootname0">Loading</p>
-    <p id = "root0"></p>
-    <p id = "rootname1"></p>
-    <p id = "root1"></p>
-    <p id = "rootname2"></p>
-    <p id = "root2"></p>
-    <p id = "rootname3"></p>
-    <p id = "root3"></p>
-    <p id = "rootname4"></p>
-    <p id = "root4"></p>
-    <p id = "rootname5"></p>
-    <p id = "root5"></p>
-    <p id = "rootname6"></p>
-    <p id = "root6"></p>
-    <p id = "rootname7"></p>
-    <p id = "root7"></p>
+    <p id="class1">Loading</p>
+    <p id="class1info"></p>
+    <p id="class2"></p>
+    <p id="class2info"></p>
+    <p id="class3"></p>
+    <p id="class3info"></p>
+    <p id="class4"></p>
+    <p id="class4info"></p>
+    <p id="class5"></p>
+    <p id="class5info"></p>
+    <p id="class6"></p>
+    <p id="class6info"></p>
+    <p id="class7"></p>
+    <p id="class7info"></p>
+    <p id="class8"></p>
+    <p id="class8info"></p>
   </div>
 export default LoginPage
